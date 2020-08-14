b1fa11879c49ed4a797e2f523c888d73dbb58b9f
sameem <42725415+MOHAMEDSAMEEM77@users.noreply.github.com>
2020-08-14 12:24:37 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,22 @@
+require('./models/db');
+
+const express =     require('express');
+const path   =       require('path');
+const exphbs   =     require('express-handlebars');
+const bodyparser   = require('body-parser');
+const CRUD = require('./CRUD/CRUD');
+
+var app = express();
+app.use(bodyparser.urlencoded({
+    extended: true
+}));
+app.use(bodyparser.json());
+app.set('views', path.join(__dirname, '/views/'));
+app.engine('hbs', exphbs({ extname: 'hbs', defaultLayout: 'mainLayout', layoutsDir: __dirname + '/views/layouts/' }));
+app.set('view engine', 'hbs');
+
+app.listen(3000, () => {
+    console.log('Express js server started at port : 3000');
+});
+
+app.use('/Vehicle', CRUD);
